cb32155bfa0238153cc5ad4eaeffe85cfa8d77d6
ishay <ishay.yulzary@gmail.com>
2025-06-06 00:04:53 +0300

    test

--- server/next.config.mjs
+++ server/next.config.mjs
@@ -3,10 +3,14 @@ const nextConfig = {
   experimental: {
     outputFileTracingRoot: process.cwd(),
   },
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Externalize Firebase to reduce bundle size
+      config.externals.push('firebase/app', 'firebase/firestore');
+    }
     config.externals = [...(config.externals || []), 'canvas', 'jsdom'];
     return config;
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
